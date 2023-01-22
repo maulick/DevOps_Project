0e1bdd1795ceb970d2a466e00b0c68affe2deb72
Maulick Metkar <44560123+maulick@users.noreply.github.com>
2023-01-22 19:02:08 +0530

    Update main.tf

--- Terraform/deploy_multiple_ec2_instance/main.tf
+++ Terraform/deploy_multiple_ec2_instance/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "devops_ec2_instance" {
   # Creates multiple identical aws ec2 instances with different names
   count = length(var.instance_names)
 
-  # All four instances will have the same ami and instance_type
+  # All the instances will have the same ami and instance_type
   ami = lookup(var.ec2_ami,var.region) 
   instance_type = var.instance_type 
   tags = {
