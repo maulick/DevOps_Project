18025924128d80dbeb6365e6bb846d5d3b3581f8
zeno <zeno@email.com>
2023-02-14 02:19:05 +0530

    Add / Modify files

--- Terraform/1_Automate Provisioning EC2 with Terraform/main.tf
+++ Terraform/1_Automate Provisioning EC2 with Terraform/main.tf
@@ -6,6 +6,9 @@ variable "vpc_cidr_block" {}
 variable "subnet_cidr_block" {}
 variable "avail_zone" {}
 variable "env_prefix" {}
+variable "my_ip" {}
+variable "instance_type" {}
+variable "public_key_location" {}
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
@@ -30,7 +33,7 @@ resource "aws_internet_gateway" "myapp-igw" {
   }
 }
 
-resource "aws_route_table" "myapp-route-table" {
+resource "aws_route_table" "myapp-route-tbl" {
   vpc_id = aws_vpc.myapp-vpc.id
   route {
     cidr_block = "0.0.0.0/0"
@@ -39,4 +42,83 @@ resource "aws_route_table" "myapp-route-table" {
   tags = {
     Name = "${var.env_prefix}-route-tbl"
   }
+}
+
+resource "aws_route_table_association" "myapp-route-tbl-assocn" {
+  subnet_id      = aws_subnet.myapp-subnet-1.id
+  route_table_id = aws_route_table.myapp-route-tbl.id
+}
+
+resource "aws_security_group" "myapp-sec-grp" {
+  name   = "myapp-sec-grp"
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+
+  tags = {
+    Name = "${var.env_prefix}-sec-grp"
+  }
+
+}
+
+data "aws_ami" "latest-ec2-ami" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_key_pair" "myapp-key-pair" {
+  key_name   = "devops-key"
+  public_key = file(var.public_key_location)
+}
+
+resource "aws_instance" "myapp-ec2" {
+  ami                         = data.aws_ami.latest-ec2-ami.id
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.myapp-subnet-1.id
+  vpc_security_group_ids      = [aws_security_group.myapp-sec-grp.id]
+  availability_zone           = var.avail_zone
+  associate_public_ip_address = true
+  key_name                    = aws_key_pair.myapp-key-pair.key_name
+
+  user_data = file("run_commands.sh")
+
+  tags = {
+    Name = "${var.env_prefix}-ec2"
+  }
+}
+
+output "aws_ec2_ami" {
+  value = data.aws_ami.latest-ec2-ami.id
+}
+
+output "aws_ec2_public_ip" {
+  value = aws_instance.myapp-ec2.public_ip
 }
